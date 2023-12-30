4ddb5a9632f1478e43858bb4aba808487d918cde
Anit Rai <anitrai747@gmail.com>
2023-12-30 12:40:45 +0530

    Updfate

---
+++ src/test/java/ReverseElementArray.java
@@ -0,0 +1,17 @@
+
+public class ReverseElementArray {
+	public static void main(String[] args) {
+		int num [] = {12,13,14};
+		
+		for(int i =0;i<num.length;i++) 
+			 	System.out.print(num[i]+ " "); 
+			 	
+		 for(int i=num.length-1;i>=0;i--) {
+			 System.out.print(num[i]);
+		 }
+		} 
+		
+
+	}
+	
+
